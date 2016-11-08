1a8c1a8a846b26a8462edba3c8cfb508631461a3
Jennifer Brown <j.a.brown.5@gmail.com>
2016-11-08 15:23:13 -0600

    feeling good about this code

---
+++ Rakefile
@@ -0,0 +1,8 @@
+require 'sinatra/activerecord'
+require 'sinatra/activerecore/rake'
+
+ActiveRecord::Base.establish_connection(
+  # allows our tasks to have access to the DB
+  :adapter => 'mysql2',
+  :database => 'superstars'
+)
--- controllers/superstar_controller.rb
+++ controllers/superstar_controller.rb
@@ -1,25 +1,76 @@
 class SuperstarController < ApplicationController #superstar controller is inheriting the application controller
   get '/' do
-    {:message => 'all items soon'}.to_json
+    # {:message => 'all items soon'}.to_json
+    # get all
+    Superstar.all.to_json
   end
 
-  get '/fab' do
-    # learn how to rule the DB
-    # use activerecord ORM b/c it's awesome
-
-    #how we create an object in the DB
-    # crud
-      # create
-      # delete
-      # update
-      # read
-    @christopher = Superstar.new
-    @christopher.talent = 'Socialite'
-    @christopher.name = 'Christopher McAwesome'
-    @christopher.outfit = 'blue sweater'
-    @christopher.save
-    @christopher.to_json
-
-    
+  # The below four are part of a restful controller
+  get '/:id' do
+    # get by id
+    @id = params[:id]
+    Superstar.find(@id).to_json
   end
+
+  post '/' do
+    # create
+    @name = params[:name]
+    @talent = params[:talent]
+    @outfit = params[:outfit]
+
+    @model = Superstar.new
+    @model.name = @name
+    @model.talent = @talent
+    @model.outfit = @outfit
+    @model.save
+
+    # place a breakpoint in ruby
+    # binding.pry
+
+    @model.to_json
+  end
+
+  patch '/:id' do
+    # update
+    @id   = params[:id]
+    @name = params[:name]
+    @talent = params[:talent]
+    @outfit = params[:outfit]
+
+    @model = Superstar.find(@id)
+    @model.name   = @name
+    @model.talent = @talent
+    @model.outfit = @outfit
+    @model.save
+    @model.to_json
+  end
+
+  delete '/:id' do
+    # delete
+    @id = params[:id]
+    @model = Superstar.find(@id)
+    @model.destroy
+    { :message => "Item of id " + @id " was removed."}.to_json
+  end
+
+
+
+  # get '/fab' do
+  #   # learn how to rule the DB
+  #   # use activerecord ORM b/c it's awesome
+  #
+  #   #how we create an object in the DB
+  #   # crud
+  #     # create
+  #     # delete
+  #     # update
+  #     # read
+  #   @christopher = Superstar.new
+  #   @christopher.talent = 'Socialite'
+  #   @christopher.name = 'Christopher McAwesome'
+  #   @christopher.outfit = 'blue sweater'
+  #   @christopher.save
+  #   @christopher.to_json
+  # end
+
 end
